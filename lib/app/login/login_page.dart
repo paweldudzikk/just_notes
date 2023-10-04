6c6ba22b99856f797acc476edf1099b3a5522b8d
Paweł Dudzik <Gamer710@wp.pl>
2023-10-04 16:18:15 +0200

    add text fields

--- lib/app/login/login_page.dart
+++ lib/app/login/login_page.dart
@@ -1,15 +1,29 @@
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatelessWidget {
-  const LoginPage({
+  LoginPage({
     super.key,
   });
+
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
       body: Center(
-        child: Text('Jesteś niezalogowany'),
+        child: Padding(
+          padding: EdgeInsets.all(12.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextField(),
+              TextField(
+                obscureText: true,
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
