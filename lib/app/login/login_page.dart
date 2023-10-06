236d7192ddd5ebf4ce5a70791c914df052697b7a
Paweł Dudzik <Gamer710@wp.pl>
2023-10-06 18:15:34 +0200

    add login by google

--- lib/app/login/login_page.dart
+++ lib/app/login/login_page.dart
@@ -1,4 +1,6 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
 class LoginPage extends StatelessWidget {
   LoginPage({
@@ -10,16 +12,75 @@ class LoginPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Center(
         child: Padding(
-          padding: EdgeInsets.all(12.0),
+          padding: const EdgeInsets.all(20.0),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              TextField(),
+              const Text(
+                'Just Notes',
+                style: TextStyle(
+                  fontSize: 50,
+                ),
+              ),
+              const SizedBox(
+                height: 50,
+              ),
               TextField(
+                decoration: const InputDecoration(
+                  hintText: ('email'),
+                ),
+                controller: emailController,
+              ),
+              TextField(
+                decoration: const InputDecoration(
+                  hintText: ('password'),
+                ),
+                controller: passwordController,
                 obscureText: true,
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              ElevatedButton(
+                onPressed: () async {
+                  await FirebaseAuth.instance.signInWithEmailAndPassword(
+                    email: emailController.text,
+                    password: passwordController.text,
+                  );
+                },
+                child: const Text('Log in'),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              ElevatedButton(
+                onPressed: () async {
+                  try {
+                    final GoogleSignInAccount? googleSignInAccount =
+                        await GoogleSignIn().signIn();
+                    if (googleSignInAccount != null) {
+                      final GoogleSignInAuthentication
+                          googleSignInAuthentication =
+                          await googleSignInAccount.authentication;
+                      final AuthCredential credential =
+                          GoogleAuthProvider.credential(
+                        accessToken: googleSignInAuthentication.accessToken,
+                        idToken: googleSignInAuthentication.idToken,
+                      );
+                      await FirebaseAuth.instance
+                          .signInWithCredential(credential);
+                    }
+                  } catch (error) {}
+                },
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Image.asset('images/google.png', height: 24.0, width: 30.0),
+                  ],
+                ),
               ),
             ],
           ),
